ac8e98adbab381e103b9697876637deaef10d302
leo <291782737@qq.com>
2020-07-02 09:30:12 +0800

    1.增加彩色日志 2.增加启动图标 3.增加网页图标

--- src/main/java/com/tfswx/Application.java
+++ src/main/java/com/tfswx/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class info {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(info.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
